129c1e70df1975aa8369379289c38c403fc1ef56
Vell <vmcilwain@me.com>
2021-10-05 19:56:15 -0400

    add fontawesome along with loading vendor assets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap'
+import "@fortawesome/fontawesome-free/js/all";
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
