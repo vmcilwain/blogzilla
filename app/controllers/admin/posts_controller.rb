6a7e74d33e3b7fbacdd8d50a5a98d64cab1a8111
Vell <vmcilwain@me.com>
2021-10-06 20:28:55 -0400

    add additional authentication tests

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,4 +1,12 @@
 class Admin::PostsController < Admin::AdminController
+  def create
+    redirect_to :index
+  end
+
+  def update
+    redirect_to :index
+  end  
+  
   def destroy
     redirect_to :index
   end
--- test/integration/admin/post_flows_test.rb
+++ test/integration/admin/post_flows_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class Admin::PostFlowsTest < ActionDispatch::IntegrationTest
   setup do
@@ -7,26 +7,25 @@ class Admin::PostFlowsTest < ActionDispatch::IntegrationTest
     # @user = create :user
   end
 
-  test "requires authentication" do
+  test 'requires authentication' do
     requires_authentication { get admin_posts_path }
     requires_authentication { get admin_post_path @post }
     requires_authentication { get new_admin_post_path }
     requires_authentication { get edit_admin_post_path @post }
     requires_authentication { delete admin_post_path @post }
 
-    # requires_authentication do
-    #   post admin_categories_path,
-    #     params: {
-    #       category: attributes_for(:category)
-    #     }
-    # end
+    requires_authentication do
+      post admin_posts_path,
+           params: {
+             post: attributes_for(:post)
+           }
+    end
     
-    # requires_authentication do
-    #   put admin_category_path(@category),
-    #     params: {
-    #       category: { name: Faker::Lorem.word }
-    #     }
-    # end
+    requires_authentication do
+      put admin_post_path(@post),
+          params: {
+            post: { title: Faker::Lorem.word }
+          }
+    end
   end
-
 end
