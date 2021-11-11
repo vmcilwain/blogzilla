ffe52b49141c11e87c6f468365a2f1b4e2d91687
Vell <vmcilwain@me.com>
2021-11-10 19:36:52 -0500

    list posts on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
+
+  def index
+    @posts = Post.order(created_at: :desc).last(6).in_groups_of(3, false)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,57 @@ module ApplicationHelper
   def error_notice
     render 'layouts/error_notice' if flash[:error].present?
   end
+
+  # Long date format
+  #
+  # @param date [Date] the date object
+  # @return day date month year - hour:minutes AM/PM
+  def long_date(date)
+    date.strftime("%A %d %B %Y - %H:%M %p") rescue 'unknown'
+  end
+
+  # Medium date format
+  #
+  # @param date [Date] the date object
+  # @return month/date/year at hour:minutes AM/PM
+  def medium_date(date)
+    date.strftime("%m/%d/%Y at %H:%M %p") rescue 'unknown'
+  end
+
+  # Another style of medium date format
+  #
+  # @param date [Date] the date object
+  # @return day/MONTH/YEAR
+  # Produces -> 18 October 2015
+  def medium_date2(date)
+    date.strftime("%d %B %Y") rescue 'unknown'
+  end
+
+  # Short date format
+  #
+  # @param date [Date] the date object
+  # @return year-month-date
+  def short_date(date)
+    date.strftime("%Y-%m-%d") rescue 'unknown'
+  end
+
+  # US date format
+  #
+  # @param date [Date] the date object
+  # @return year-month-date
+  def us_date(date)
+    date.strftime("%m/%d/%Y at %H:%M %p") rescue 'unknown'
+  end
+  
+  def boolean_to_text(value)
+    case value
+    when true
+      'Yes'
+    when false
+      'No'
+    else
+      'Unknown Value'
+    end
+  end
+
 end
