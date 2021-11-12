61d093d08c9fc323a096a11a0125eb62565b3384
Vell <vmcilwain@me.com>
2021-11-11 21:09:49 -0500

    add create post test

--- test/system/admin/posts_test.rb
+++ test/system/admin/posts_test.rb
@@ -8,4 +8,20 @@ class Admin::PostsTest < ApplicationSystemTestCase
   
     assert_selector "h1", text: "Posts Administration"
   end
+
+  test 'creating a post as an adminsitrator' do
+    new_session admin_user
+
+    visit new_admin_post_url
+
+    assert_selector 'h1', text: 'New Post'
+
+    fill_in :post_title, with: words
+    fill_in_rich_text_area 'post[content]', with: paragraphs
+
+    click_button 'Save'
+
+    assert_success_alert
+    assert_equal admin_post_url(Post.last), current_url
+  end
 end
