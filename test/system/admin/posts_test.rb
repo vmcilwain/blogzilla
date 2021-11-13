a1649d3ad9269b2e7826df0c691e7ab1c0d53081
Vell <vmcilwain@me.com>
2021-11-13 11:35:32 -0500

    add successful update and destroy test

--- test/system/admin/posts_test.rb
+++ test/system/admin/posts_test.rb
@@ -24,4 +24,37 @@ class Admin::PostsTest < ApplicationSystemTestCase
     assert_success_alert
     assert_equal admin_post_url(Post.last), current_url
   end
+
+  test 'updating a post as an administrator' do
+    new_session admin_user
+
+    visit edit_admin_post_url(post)
+
+    assert_selector 'h1', text: 'Edit Post'
+
+    fill_in :post_title, with: words
+
+    click_button :Save
+
+    assert_success_alert
+    assert_equal admin_post_url(post), current_url
+  end
+
+  test 'destroying a post as an administrator' do
+    new_session admin_user
+    
+    visit admin_post_url(post)
+
+    click_link :Delete
+
+    click_ok
+
+    assert_success_alert
+    assert_current_url admin_posts_url
+  end
+
+  def post
+    @post ||= create :post
+  end
+  
 end
